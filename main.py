7541cedf53139557ea27297bdf0c2e0a472a8c8f
Marcos Rodriguez <marcosjose25@users.noreply.github.com>
2024-06-07 13:37:24 +0000

    Update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Write your code below this line 👇
+print("Hello World!\nHello World!\nHello World!")
+
 print('Welcome to the Band Name Generator.')
-print('What\'s the name of the city you grew up in?')
-input(cityname)
+city = input('What\'s the name of the city you grew up in? ')
+pet = input("What's your pet's name? ")
+print('Your band name could be: ' + city + ' ' + pet)
